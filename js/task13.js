d447326aac06a4e3a194005c9b74709efa46c176
misha <golovchuk.misha@gmail.com>
2021-11-30 21:24:06 +0200

    modifield

--- js/task13.js
+++ js/task13.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// if (4 == 5) {
-//   console.log("Ok");
-// } else {
-// console.log('error')
-// }
+if (4 == 5) {
+  console.log("Ok");
+} else {
+console.log('error')
+}
 
 const num = 50;
 
-// if (num < 49) {
-//     console.log("error")
-// } else if (num > 100) {
-//     console.log("many")
-// } else {
-//     console.log('Ok!')
-// }
+if (num < 49) {
+    console.log("error")
+} else if (num > 100) {
+    console.log("many")
+} else {
+    console.log('Ok!')
+}
 
-// num === 50 ? console.log("Ok!") : console.log("error");
+num === 50 ? console.log("Ok!") : console.log("error");
 
 switch (num) {
   case 49:
