78abdf7951dfbfc022181e93a83e6312a503a51a
misha <golovchuk.misha@gmail.com>
2021-11-30 16:45:07 +0200

    add task 12

---
+++ js/task12(1).js
@@ -0,0 +1,17 @@
+'use strict'
+
+let numberOfFilms = prompt("сколько фильмов вы посмотрели?", "")
+let numderOfFilms2 = prompt("один из последних просмотренных фильмов?", "")
+let numderOfFilms3 = prompt("какая оценка фильма?", "");
+
+
+ const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    generes : [],
+    private : false
+};
+
+personalMovieDB.movies[numderOfFilms2] = numderOfFilms3;
+console.log(personalMovieDB);
